826d6c6dda5370e8e58d037150201d433d314ef0
animaster01 <jmedina@CND4215JLR.campus.ltu.edu>
2016-01-28 20:28:39 -0500

    changes

---
+++ IGSProjectUnnamed/Assets/Script/BoulderFallScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoulderFallScript : MonoBehaviour {
+
+    public float roofHeight = 10;
+    public Color ColorA;
+    public Color ColorB;
+    public SpriteRenderer renderer;
+    public int numberOfWarnings = 3;
+    public float timeOfBlip = 0.5f;
+    public Transform Boulder;
+    public AudioClip warningSound;
+    public bool audibleWarning = false;
+
+    GameManager manager;
+
+	// Use this for initialization
+	void Start () {
+        manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<GameManager>();
+        StartCoroutine(warning());
+	}
+
+    IEnumerator warning()
+    {
+        for (int a = 0; a < numberOfWarnings; a++)
+        {
+            renderer.color = ColorA;
+            yield return new WaitForSeconds(timeOfBlip);
+            
+            if (audibleWarning)
+                manager.SFX.PlaySound(warningSound);
+            
+            renderer.color = ColorB;
+            yield return new WaitForSeconds(timeOfBlip);
+        }
+
+        //Note, we may need to change the z axis later because I'm assuming here that the coordinates are always gonna be negative
+        Transform boulder = Instantiate(Boulder, new Vector3(transform.position.x, roofHeight, transform.position.y), Quaternion.identity) as Transform;
+        boulder.GetComponent<BoulderScript>().objectiveHeight = transform.position.y;
+        Destroy(gameObject);
+    }
+}
---
+++ IGSProjectUnnamed/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float XVelocity = 1f;
+    public float YVelocity = 1f;
+    public Vector2 minPos;
+    public Vector2 maxPos;
+    public bool squareMovementX = false;
+    public bool squareMovementY = false;
+
+    private float VerticalD;
+    private float HorizontalD;
+
+    GameManager manager;
+
+    void Start()
+    {
+        manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<GameManager>();
+    }
+
+	void Update () {
+
+        if (!manager.gameOver)
+        {
+            if (!squareMovementX)
+            {
+                HorizontalD = XVelocity * Time.deltaTime * Input.GetAxisRaw("Horizontal");
+
+                if (transform.position.x + HorizontalD < minPos.x)
+                    HorizontalD = 0;
+                else if (transform.position.x + HorizontalD > maxPos.x)
+                    HorizontalD = 0;
+
+                transform.Translate(new Vector3(HorizontalD, 0, 0));
+            }
+            else if (Input.GetButtonDown("Horizontal") && squareMovementX)
+            {
+                HorizontalD = Mathf.RoundToInt(Input.GetAxisRaw("Horizontal"));
+
+                if (transform.position.x + HorizontalD < minPos.x)
+                    HorizontalD = 0;
+                else if (transform.position.x + HorizontalD > maxPos.x)
+                    HorizontalD = 0;
+
+                transform.Translate(new Vector3(HorizontalD, 0, 0));
+                Mathf.RoundToInt(transform.position.x);
+            }
+
+
+            if (!squareMovementY)
+            {
+                VerticalD = YVelocity * Time.deltaTime * Input.GetAxisRaw("Vertical");
+
+                if (transform.position.y + VerticalD < minPos.y)
+                    VerticalD = 0;
+                else if (transform.position.y + VerticalD > maxPos.y)
+                    VerticalD = 0;
+
+                transform.Translate(new Vector3(0, VerticalD, 0));
+            }
+            else if (Input.GetButtonDown("Vertical") && squareMovementY)
+            {
+                VerticalD = Mathf.RoundToInt(Input.GetAxisRaw("Vertical"));
+
+                if (transform.position.y + VerticalD < minPos.y)
+                    VerticalD = 0;
+                else if (transform.position.y + VerticalD > maxPos.y)
+                    VerticalD = 0;
+
+                transform.Translate(new Vector3(0, VerticalD, 0));
+                Mathf.RoundToInt(transform.position.y);
+            }
+        }
+
+
+	}
+}
