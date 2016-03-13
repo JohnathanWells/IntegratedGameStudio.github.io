14b4f62e5e6c51f5add79f5d7acb4189536691a1
JohnathanWells <slaifir@hotmail.com>
2016-03-13 07:08:11 -0400

    Got rid of some stuff we didn't need. Created folders to make organizing stuff easier. Created a level selection (That does some weird stuff but we will fix that later

---
+++ BashingBashrans/Assets/Script/UIScripts/levelSelection.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelSelection : MonoBehaviour{
+
+    public void loadLevel(string name)
+    {
+        Application.LoadLevel(name);
+    }
+}
